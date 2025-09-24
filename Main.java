20d90c1dbb2e97800b8bf88897c7abc499e8e589
Priya Maurya <priyamaurya1612@gmail.com>
2025-09-24 23:56:27 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,84 @@
-import java.util.Arrays;
-
-public class Main {
+import java.util.Scanner;
+class Main {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-         bubble(arr);
-        System.out.println(Arrays.toString(arr));
+        char[][] board = new char[3][3];
+        for(int row = 0 ; row < board.length ; row++){
+            for(int col = 0 ; col < board[row].length ; col++){
+                //initialise as empty
+                board[row][col] = ' ';
+            }
+        }
 
-    }
-        //run the steps n-1 times
-        static void bubble( int[] arr){
-            for (int i = 0; i < arr.length; i++) {
-                //for each step, max item will come at last respective index
-                for (int j = 1; j < arr.length - i; j++) {
-                    if(arr[j] < arr[j-1]){
-                        //swap
-                        int temp = arr[j];
-                        arr[j] = arr[j-1];
-                        arr[j-1] = temp;
-                    }
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner sc = new Scanner(System.in);
 
+        while(!gameOver){    //game not over
+            //print the board
+            printBoard(board);
+            System.out.println("Player " + player + " Enter: ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+
+
+            if(board[row][col] == ' '){
+                board[row][col] = player; // place the element
+                gameOver = haveWon(board , player);
+                if(gameOver){
+                    System.out.println("PLayer " + player + " has won: " );
                 }
+                else {
+//                    if(player == 'X'){
+//                        player= 'O';
+//                    }else{
+//                        player = 'X';
+//                    }
 
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            }else {
+                System.out.println("Invalid Move. Try again.");
             }
 
         }
+        printBoard(board);
+
     }
+
+
+    public static void printBoard(char[][] board){
+        //
+        for(int row = 0 ; row < board.length ; row++){
+            for(int col = 0 ; col < board[row].length ; col++){
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean haveWon(char[][] board, char player) {
+        // check the rows
+        for(int row = 0 ; row < board.length ; row++){
+            if(board[row][0] == player && board[row][1] == player && board[row][2] == player){
+                return true;
+            }
+        }
+        // check for col
+        for(int col = 0 ; col < board.length ;col++){
+            if(board[0][col] == player && board[1][col] == player && board[2][col] == player){
+                return true;
+            }
+        }
+
+        // for diagonal
+        if(board[0][0] == player && board[1][1] == player && board[2][2] == player){
+            return true;
+        }
+
+        if(board[0][2] == player && board[1][1] == player && board[2][0] == player){
+            return true;
+        }
+
+        return false;
+    }
+
+}
